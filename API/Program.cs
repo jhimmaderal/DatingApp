95bcdb50ea0a42b2ddb06df755894b44a7ccb950
jhimmaderal <jhimmaderal@gmail.com>
2023-08-31 08:40:37 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,18 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 // Add Sql Connection String
 builder.Services.AddDbContext<DataContext>(opt => 
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(); // Add Services for API
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//Middleware
+// Allow Access multiorigin webserver to api
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
