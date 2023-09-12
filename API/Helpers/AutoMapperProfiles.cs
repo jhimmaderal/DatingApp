3ab9a23d007ef1a7df5f24eb95dfeababdd6070d
jhimmaderal <jhimmaderal@gmail.com>
2023-09-12 17:17:58 +0300

    End of section 12

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -14,6 +14,7 @@ namespace API.Helpers
 
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
+            CreateMap<RegisterDto, AppUser>();
         }
     }
 }
--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -55,7 +55,7 @@ export class PhotoEditorComponent implements OnInit {
 
   deletePhoto(photoId: number) {
     this.memberService.deletePhoto(photoId).subscribe({
-      next: _ => { 
+      next: _ => {
         if (this.member) {
           this.member.photos = this.member.photos.filter(x => x.id !== photoId);
         }
@@ -82,6 +82,11 @@ export class PhotoEditorComponent implements OnInit {
       if (response) {
         const photo = JSON.parse(response);
         this.member?.photos.push(photo);
+        if (photo.isMain && this.user && this.member) {
+          this.user.photoUrl = photo.url;
+          this.member.photoUrl = photo.url;
+          this.accountService.setCurrentUser(this.user);
+        }
       }
     }
   }
